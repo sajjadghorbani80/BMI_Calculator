f72e9ee00c813f60bd2cb4792a21602e0fd10ac5
sajjad ghorbani <sajjadr2001@gmail.com>
2023-02-08 11:42:52 +0330

    Create frontend home page add fonts to project

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+app.use('/statics',express.static(path.join(__dirname,'statics')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
-    res.send('hi');
+    res.render(path.join(__dirname,'views/index.ejs'));
 })
 
 app.listen(port, ()=> {
