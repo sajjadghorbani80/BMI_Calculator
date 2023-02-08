463d6ad8dcc1a9af5fde2be177b42764ee447a38
sajjad ghorbani <sajjadr2001@gmail.com>
2023-02-08 13:03:53 +0330

    handle request from frontend -set data from inputs to an object -send request to api and receive response -show response in html page

---
+++ statics/js/script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById("submit");
+button.addEventListener('click', function(){
+    const data = {"height": document.getElementById("height-input").value,
+    "weight": document.getElementById("weight-input").value};
+
+    const request = fetch('/bmi',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json()).then(data => {
+        document.getElementById("show-result").innerHTML = `مقدار bmi شما ${data.bmi} است.`
+    });
+
+})
+
+
+JSON.stringify({ "height": document.getElementById("height-input").value,
+        "weight": document.getElementById("weight-input").value,
+    })
